4b46ea5b0260624c7183512cacf7bbe002fc247d
zaryabshakir <zaryabshakir99@gmail.com>
2021-08-03 14:27:27 +0500

    comments added keypair

--- createKeyPair.go
+++ createKeyPair.go
@@ -20,28 +20,35 @@ type PK struct {
 	PrivateKey string `json:"pk"`
 }
 
+// Create fresh keyPair
 func createKeyPair(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	account := types.NewAccount()
+	//Creating a fresh KeyPair
 	keypair := KeyPair{Status: true, AccountAddress: account.PublicKey.ToBase58(), PrivateKey: account.PrivateKey}
 	json.NewEncoder(w).Encode(keypair)
 }
 
+//get Address by privateKey
 func pKToAddress(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var privkeybytes PK
 	json.Unmarshal(reqBody, &privkeybytes)
 	w.Header().Set("Content-Type", "application/json")
+	// Decoding base64 Private Key to Bytes
 	privBytes, err := Base64Decode([]byte(privkeybytes.PrivateKey))
 	if err != nil {
 		fatal := Error{Status: false, Message: string(err.Error())}
 		json.NewEncoder(w).Encode(fatal)
 		return
 	}
+	//Getting Account Address by PrivateKey Bytes
 	account := types.AccountFromPrivateKeyBytes(privBytes)
 	keypair := KeyPair{Status: true, AccountAddress: account.PublicKey.ToBase58(), PrivateKey: account.PrivateKey}
 	json.NewEncoder(w).Encode(keypair)
 }
+
+//Connection with Client Testing
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	fatal := Error{Status: true, Message: "hello solana "}

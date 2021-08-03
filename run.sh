d79c26fb9c39bcce969ee592778fe42de1b52524
zaryabshakir <zaryabshakir99@gmail.com>
2021-08-03 15:32:18 +0500

    script updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-go run createAndSendTransaction.go    createKeyPair.go    getBalance.go  utils.go  server.go   
+go run createAndSendTransaction.go    createKeyPair.go    getBalance.go  utils.go tokensTransfer.go server.go   
 
 
 

8ac7b76a9e46ea8cace000ab02941c854acd09fd
zaryabshakir <zaryabshakir99@gmail.com>
2021-08-03 15:31:13 +0500

    get aTa added

--- createKeyPair.go
+++ createKeyPair.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/portto/solana-go-sdk/common"
 	"github.com/portto/solana-go-sdk/types"
 )
 
@@ -18,6 +19,18 @@ type KeyPair struct {
 // PK struct
 type PK struct {
 	PrivateKey string `json:"pk"`
+}
+
+// ATAParams struct
+type ATAParams struct {
+	Address string `json:"address"`
+	Mint    string `json:"mint"`
+}
+
+// ATAResponse struct
+type ATAResponse struct {
+	Status     bool   `json:"status"`
+	AtaAddress string `json:"ataAddress"`
 }
 
 // Create fresh keyPair
@@ -46,6 +59,42 @@ func pKToAddress(w http.ResponseWriter, r *http.Request) {
 	account := types.AccountFromPrivateKeyBytes(privBytes)
 	keypair := KeyPair{Status: true, AccountAddress: account.PublicKey.ToBase58(), PrivateKey: account.PrivateKey}
 	json.NewEncoder(w).Encode(keypair)
+}
+
+//Get ATA account
+func getATA(w http.ResponseWriter, r *http.Request) {
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var params ATAParams
+	json.Unmarshal(reqBody, &params)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// Basic Validation
+	if params.Address == "" {
+		fatal := Error{Status: false, Message: "Required Param , Address"}
+		json.NewEncoder(w).Encode(fatal)
+		return
+
+	}
+	if params.Mint == "" {
+		fatal := Error{Status: false, Message: "Required Param , Mint Address"}
+		json.NewEncoder(w).Encode(fatal)
+		return
+
+	}
+
+	//Getting  ATA account
+	ata, _, err := common.FindAssociatedTokenAddress(common.PublicKeyFromString(params.Address), common.PublicKeyFromString(params.Mint))
+	if err != nil {
+		fatal := Error{Status: false, Message: "find SenderAta error, err: " + string(err.Error())}
+		json.NewEncoder(w).Encode(fatal)
+		return
+	}
+	ataAddress := ata.ToBase58()
+
+	ataRes := ATAResponse{Status: true, AtaAddress: ataAddress}
+	json.NewEncoder(w).Encode(ataRes)
 }
 
 //Connection with Client Testing
